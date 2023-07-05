2c99338ac77e11e65af5d10ed6e28f1abca1bfb5
bmandracchia <biagio.mandracchia@gmail.com>
2023-07-05 18:53:24 +0200

    noise2flow added

--- Noise2Model/core.py
+++ Noise2Model/core.py
@@ -6,12 +6,14 @@ __all__ = ['get_gt', 'get_noisy_pair']
 # %% ../nbs/00_core.ipynb 3
 from fastai.vision.all import *
 from fastai.data.all import *
+import torch.nn.functional as F
 
+# %% ../nbs/00_core.ipynb 4
 def get_gt(path, gt_file_name="avg50.png"): 
     def _fn(fn): return Path(path/"gt")/f"{parent_label(fn)}"/gt_file_name
     return _fn
 
-# %% ../nbs/00_core.ipynb 5
+# %% ../nbs/00_core.ipynb 6
 from random import randint
 
 def get_noisy_pair(fn):
